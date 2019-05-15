303ec9f237ee5eeda0b43cb3711357fc2a169a89
yabinmeng <yabinmeng@gmail.com>
2019-05-14 21:55:32 -0500

    Update dse_tlsv12.java

--- src/main/java/com/example/dse_tlsv12.java
+++ src/main/java/com/example/dse_tlsv12.java
@@ -45,7 +45,9 @@ public class dse_tlsv12 {
             // NOTE: ERROR with java.lang.IllegalArgumentException: TLS
             //       has to be TLSv1, TLSv1.1, TLSv1.2
             //builder.protocols("TLS");
-            // NOTE: can be multiples. But "TLSv1.2" only works fine.
+            // NOTE: can be multiples. But,
+            //       1) "TLSv1.2" only works fine with both DSE nodes
+            //       2) "TLSv1" and "TLSv1.1" does NOT work with TLSv1.2 specific DSE node
             //builder.protocols("TLSv1", "TLSv1.1", "TLSv1.2");
             builder.protocols("TLSv1.2");
 
@@ -84,7 +86,6 @@ public class dse_tlsv12 {
 
             SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(null, tm, null);
-
 
             sslOptions = RemoteEndpointAwareJdkSSLOptions.builder()
                 .withSSLContext(sslContext)
